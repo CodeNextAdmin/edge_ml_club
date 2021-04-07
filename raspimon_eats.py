57328cb37cf4f51b97fad23c80b3d2169ddb1ca4
Scott Main <smain@google.com>
2021-04-07 16:26:44 -0700

    Remove code from the project scripts as a starting point for the club

--- raspimon_eats.py
+++ raspimon_eats.py
@@ -1,7 +1,6 @@
 from sense_hat import SenseHat
 from time import sleep
 from random import randint
-import voice
 
 sense = SenseHat()
 
@@ -35,17 +34,8 @@ def move():
     else:
       next[0] = raspimon[0] - 1
 
-  elif direction == "down":
-    if raspimon[1] + 1 == 8:
-      next[1] = 0
-    else:
-      next[1] = raspimon[1] + 1
+  # TODO: Fill in the "up" and "down" conditions
 
-  elif direction == "up":
-    if raspimon[1] - 1 == -1:
-      next[1] = 7
-    else:
-      next[1] = raspimon[1] - 1
 
   # Update the position
   sense.set_pixel(raspimon[0], raspimon[1], BLANK)
@@ -61,16 +51,6 @@ def joystick_moved(event):
   global direction
   direction = event.direction
 
-def change_direction(label):
-  global direction
-  if label.endswith("up"):
-    direction = "up"
-  elif label.endswith("down"):
-    direction = "down"
-  elif label.endswith("left"):
-    direction = "left"
-  elif label.endswith("right"):
-    direction = "right"
 
 def add_berry():
   x = randint(0, 7)
@@ -79,22 +59,16 @@ def add_berry():
   sense.set_pixel(x, y, RED)
   berries.append(new)
 
+
 # Main program ------------------------
+
 sense.clear()
 sense.stick.direction_any = joystick_moved
-listener = voice.AudioClassifier(model_file=voice.VOICE_MODEL,
-                                 labels_file=voice.VOICE_LABELS,
-                                 audio_device_index=2)
 
 for _ in range(5):
   add_berry()
 
 while True:
-  command = listener.next(block=False)
-  if command:
-    label, score = command
-    change_direction(label)
-    print(label, score)
   move()
   sleep(DELAY)
   if len(berries) == 0:
--- raspimon_sees_faces.py
+++ raspimon_sees_faces.py
@@ -1,17 +1,3 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from sense_hat import SenseHat
 from time import sleep
 from math import floor
@@ -19,8 +5,8 @@ import vision
 from detect import BBox
 from bestiary import Volt
 
-FOV_COLUMNS = 3
-FOV_ROWS = 2
+FOV_COLUMNS =
+FOV_ROWS =
 
 # initialize SenseHat instance and clear the LED matrix
 sense = SenseHat()
@@ -47,41 +33,24 @@ def get_fov_bboxes(image_size):
 def get_location(bbox, image_size):
   """Returns the index position of the cell where the given BBox
      currently appears. The image_size is (width, height) """
-  fov_bboxes = get_fov_bboxes(image_size)
+
   # Create a center-point box (a small box representing the face center)
-  pbox = BBox(bbox.xmin, bbox.ymin, bbox.xmin + 1, bbox.ymin + 1)
-  pbox = pbox.translate(floor(bbox.width/2), floor(bbox.height/2))
-  # Check which cell the currently intersects with the face center
-  for index, fov_box in enumerate(fov_bboxes):
-    if BBox.iou(pbox, fov_box) > 0:
-      return index
+
+  # Get coordinates for each box in the raspimon field of view (FOV)
+
+  # Check which box in FOV currently intersects with the face center
+
 
 
 def react_to_faces(faces):
   """Redraw the raspimon in response to detected faces."""
-  if (len(faces) == 1):
-    face_loc = get_location(faces[0].bbox, (640, 480))
-    if face_loc == 0:
-      sense.set_pixels(Volt.LOOK_UP_LEFT)
-    elif face_loc == 1:
-        sense.set_pixels(Volt.LOOK_UP)
-    elif face_loc == 2:
-        sense.set_pixels(Volt.LOOK_UP_RIGHT)
-    elif face_loc == 3:
-        sense.set_pixels(Volt.LOOK_DOWN_LEFT)
-    elif face_loc == 4:
-        sense.set_pixels(Volt.LOOK_DOWN)
-    elif face_loc == 5:
-        sense.set_pixels(Volt.LOOK_DOWN_RIGHT)
+  # First get the location of the face (one of six positions)
 
 
-# load the neural network model (obfuscates use of TF and Edge TPU)
-detector = vision.Detector(vision.FACE_DETECTION_MODEL)
+# Main program ------------------------
 
-# run a loop to run the model in real-time
-for frame in vision.get_frames():
-  faces = detector.get_objects(frame)
+# Load the neural network model
+
+# Run a loop to get images and process them in real-time
   # Draw bounding boxes on the frame and display it
-  vision.draw_objects(frame, faces)
   # Pass faces to function that controls raspimon
-  react_to_faces(faces)
