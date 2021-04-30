c0059f2a41d59c954bf7ec035ac3d390c7127777
Scott Main <smain@google.com>
2021-04-29 18:58:05 -0700

    refactor the starting code, add more comments

--- raspimon_sees_faces.py
+++ raspimon_sees_faces.py
@@ -1,21 +1,26 @@
 from sense_hat import SenseHat
-from time import sleep
-from math import floor
 import vision
 from pycoral.adapters.detect import BBox
 from bestiary import Volt
 
+### Set the number of squares for the "field of view" (FOV)
 FOV_COLUMNS =
 FOV_ROWS =
 
-# initialize SenseHat instance and clear the LED matrix
-sense = SenseHat()
-sense.set_pixels(Volt.LOOK_UP)
-
+# Set the available poses; length must match the total FOV squares
+VOLT_POSES = [
+  Volt.LOOK_UP_LEFT,
+  Volt.LOOK_UP,
+  Volt.LOOK_UP_RIGHT,
+  Volt.LOOK_DOWN_LEFT,
+  Volt.LOOK_DOWN,
+  Volt.LOOK_DOWN_RIGHT
+]
 
 def get_fov_bboxes(image_size):
   """Returns a list of BBox objects representing each cell in the
-     raspimon's field of view (FOV)."""
+     Raspimon's field of view (FOV). These are in sequence from
+     left-to-right and top-to-bottom (top-left is first)."""
   cell_width = image_size[0] / FOV_COLUMNS
   cell_height = image_size[1] / FOV_ROWS
   bboxes = []
@@ -30,27 +35,74 @@ def get_fov_bboxes(image_size):
   return bboxes
 
 
+def react_to_faces(faces):
+  """Redraw the raspimon in response to the face location in FOV squares."""
+  if (len(faces) == 1):
+    # Get the location of the face (one of six positions)
+    face_loc = get_location(faces[0].bbox, vision.VIDEO_SIZE)
+    # Set the Raspimon pose
+    sense.set_pixels(VOLT_POSES[face_loc])
+
+
 def get_location(bbox, image_size):
   """Returns the index position of the cell where the given BBox
      currently appears. The image_size is (width, height) """
 
-  # Create a center-point box (a small box representing the face center)
+  # Get the center point for the face bounding-box
+  face_x, face_y = get_center_point(bbox)
 
   # Get coordinates for each box in the raspimon field of view (FOV)
+  fov_bboxes = get_fov_bboxes(image_size)
 
-  # Check which box in FOV currently intersects with the face center
+  # Find which FOV box currently holds the center point
+  for index, fov_bbox in enumerate(fov_bboxes):
+    if is_point_in_box(face_x, face_y, fov_bbox):
+      return index
+
+### FIX IN THIS FUNCTION
+def is_point_in_box(x, y, bbox):
+    """
+    Check if the given (x,y) point lies within the given box.
+
+    Args:
+      x (int): The X-coordinate for the point
+      y (int): The Y-coordinate for the point
+      bbox (BBox): A `BBox` (bounding box) object
+    Returns:
+      True if the point is inside the bounding box; False otherwise
+    """
+    if x > 0 and y > 0:
+        return True
+    return False
+
+### FIX IN THIS FUNCTION
+def get_center_point(bbox):
+    """
+    Return the center point for the given box, as (x,y) position.
+
+    Args:
+      bbox (BBox): A `BBox` (bounding box) object
+    Returns:
+      A tuple as (x,y), representing the center of the box
+    """
+    x_middle = 42
+    y_middle = 42
+
+    return (x_middle, y_middle)
 
 
+### FINISH THIS CODE ------------------------
 
-def react_to_faces(faces):
-  """Redraw the raspimon in response to detected faces."""
-  # First get the location of the face (one of six positions)
-
-
-# Main program ------------------------
+# initialize SenseHat instance and set raspimon
+sense = SenseHat()
+sense.set_pixels(Volt.LOOK_UP)
 
 # Load the neural network model
 
 # Run a loop to get images and process them in real-time
+
   # Draw bounding boxes on the frame and display it
+
+  # Experiment code:
+
   # Pass faces to function that controls raspimon
