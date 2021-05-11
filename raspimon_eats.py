20b85793be1ccedcd99c16ab4b1b660dd4ec6161
Scott Main <smain@google.com>
2021-05-10 18:36:02 -0700

    refactor functions for new coding procedure

--- raspimon_eats.py
+++ raspimon_eats.py
@@ -2,6 +2,7 @@ from sense_hat import SenseHat
 from sense_hat import stick
 from time import sleep
 from random import randint
+import voice
 
 # Constants
 D_UP = stick.DIRECTION_UP
@@ -16,10 +17,10 @@ DELAY = 1.0
 # Variables
 raspimon = [2,4]
 berries = []
-direction = D_UP
+direction = D_RIGHT
 
 
-def move():
+def move_and_draw():
   """Performs all LED updates in each game cycle, including eating berries."""
 
   global raspimon
@@ -27,22 +28,27 @@ def move():
   # Copy the current position
   next = list(raspimon)
 
-  # Find the next pixel in the direction the slug is currently moving
+  # Find the next pixel in the direction the Raspimon is currently moving
   if direction == D_UP:
     if raspimon[1] - 1 == -1:
       next[1] = 7
     else:
       next[1] = raspimon[1] - 1
-
   elif direction == D_DOWN:
     if raspimon[1] + 1 == 8:
       next[1] = 0
     else:
       next[1] = raspimon[1] + 1
-
-  ###
-  ### Fill in the "left" and "right" conditions
-  ###
+  elif direction == D_LEFT:
+    if raspimon[0] - 1 == -1:
+      next[0] = 7
+    else:
+      next[0] = raspimon[0] - 1
+  elif direction == D_RIGHT:
+    if raspimon[0] + 1 == 8:
+      next[0] = 0
+    else:
+      next[0] = raspimon[0] + 1
 
   # Update the position
   sense.set_pixel(raspimon[0], raspimon[1], BLANK)
@@ -53,30 +59,6 @@ def move():
     berries.remove(next)
 
   raspimon = next
-
-
-def joystick_moved(event):
-  """
-  Update the Raspimon's direction based on callback from joystick events.
-  Args:
-    event: An `InputEvent` from SenseHat.stick
-  """
-  global direction
-  direction = event.direction
-
-
-### Finish this function
-def change_direction(label):
-  """
-  Change the Raspimon's direction based on voice command.
-  Args:
-    label (str): The recognized voice command label
-  """
-  global direction
-  if label.endswith("up"):
-    direction = D_UP
-  elif label.endswith("down"):
-    direction = D_DOWN
 
 
 def generate_berries():
@@ -90,29 +72,50 @@ def generate_berries():
     berries.append([x,y])
 
 
+def respond_to_joystick(event):
+  """
+  Update the Raspimon's direction based on a callback from joystick events.
+  Args:
+    event: An `InputEvent` from SenseHat.stick
+  """
+  global direction
+  if event.direction == D_UP:
+    direction = D_UP
+  elif event.direction == D_DOWN:
+    direction = D_DOWN
+  elif event.direction == D_LEFT:
+    direction = D_LEFT
+  elif event.direction == D_RIGHT:
+    direction = D_RIGHT
+
+
+### 3. FILL IN THIS FUNCTION
+def respond_to_voice(command):
+  """
+  Update the Raspimon's direction based on a voice command.
+  Args:
+    command: A voice command, as given by AudioClassifier.next()
+  """
+
 
 # Main program ------------------------
 
 sense = SenseHat()
 sense.clear()
 
-# Set callback for joystick events
-sense.stick.direction_any = joystick_moved
-
-###
-### Add listener for speech detection model
-###
-
-# Add berries to screen
+# Add berries to the screen
 generate_berries()
+
+# Set callback for joystick events
+sense.stick.direction_any = respond_to_joystick
+
+### 1. CREATE LISTENER FOR SPEECH CLASSIFIER
 
 # Main game loop
 while True:
-  ###
-  ### Respond to speech detection results
-  ###
+  ### 2. RESPOND TO SPEECH CLASSIFICATIONS
 
-  move()
+  move_and_draw()
   sleep(DELAY)
 
   if len(berries) == 0:
